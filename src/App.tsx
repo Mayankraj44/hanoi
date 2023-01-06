22c181d8964e6d92c4695ece4e6b0cdee6878fa7
Mayank Raj <lui.chang420@gmail.com>
2023-01-06 19:07:28 +0530

    structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [pegs, setPegs] = useState([[0, 1, 2], [], []]);
+  return (
+    <div className="pegs-container">
+      {pegs?.map((peg) => (
+        <div className="peg">
+          <div className="stand" />
+          <div className="disc-container">
+            {peg.map((disc) => (
+              <span className="disc"></span>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
