fcbb27562fd3778455e7567635f5224ddf8f38f4
Mayank Raj <lui.chang420@gmail.com>
2023-01-06 22:10:00 +0530

    added colors to discs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import randomColor from "randomcolor";
 
 const NUMBER = 3;
+const colorArray = randomColor({ count: NUMBER });
 
 function App() {
   const winningArray = [...Array(NUMBER).keys()].reverse();
   const [pegs, setPegs] = useState<Array<Array<number>>>([[2, 1, 0], [], []]);
   const [discPicked, setDiscPicked] = useState<number | undefined>(undefined);
-
   function wonOrNot() {
     let targetArray = JSON.stringify(pegs[2]);
     let winningString = JSON.stringify(winningArray);
@@ -36,12 +37,21 @@ function App() {
       setPegs([...clone]);
     }
   }
+  function reset() {
+    const resetData = [[...winningArray], [], []];
+    setPegs([...resetData]);
+    setDiscPicked(undefined);
+  }
 
   return (
     <>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
       <div className="pegs-container">
         {pegs?.map((peg, pegNum) => (
           <div
+            key={pegNum}
             className="peg"
             style={{
               cursor:
@@ -53,11 +63,13 @@ function App() {
           >
             <div className="stand" />
             <div className="disc-container">
-              {peg.map((disc) => (
+              {peg.map((disc, ind) => (
                 <span
+                  key={ind}
                   className="disc"
                   style={{
                     width: 50 + 25 * disc + "px",
+                    backgroundColor: colorArray[disc],
                   }}
                 ></span>
               ))}
